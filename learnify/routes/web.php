85878e15e746a0f681c44e891bd3a5615402f463
Vincentius Justin Osvaldy <mrggguyyy@gmail.com>
2025-10-12 20:38:24 +0700

    Update web.php

--- learnify/routes/web.php
+++ learnify/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\MyCourseController;
+use App\Http\Controllers\MaterialsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/MyCourse', [MyCourseController::class, 'MyCourse'])->name('MyCourse');
+
+Route::get('/Materials', [MaterialsController::class, 'Materials'])->name('Materials');
