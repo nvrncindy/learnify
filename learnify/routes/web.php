506597184393a68b449327e2385109517ceb998a
Rafaelsanyoto <rafaelsanyoto2187@gmail.com>
2025-10-12 17:08:54 +0700

    Update web.php

--- learnify/routes/web.php
+++ learnify/routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [CourseController::class, 'index'])->name('home');
+Route::get('/courses', [CourseController::class, 'index'])->name('courses.index');
