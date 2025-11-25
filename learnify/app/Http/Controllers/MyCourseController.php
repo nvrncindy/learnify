1e6c6c6a6bd1ae2b9cf7bad8077c69f333ae703c
Vincentius Justin Osvaldy <mrggguyyy@gmail.com>
2025-11-26 01:50:11 +0700

    Update MyCourse method to include courses
    Fetch all courses and pass them to the MyCourse view.

--- learnify/app/Http/Controllers/MyCourseController.php
+++ learnify/app/Http/Controllers/MyCourseController.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Course;
 
 use Illuminate\Http\Request;
 
 class MyCourseController extends Controller
 {
     public function MyCourse(){
-        return view("MyCourse");
+        $courses = Course::all();
+
+        return view("MyCourse", compact('courses'));
     }
 }
