843ab97b5dc7ace6110e3fc0e6021d108c9ec28e
Rafaelsanyoto <rafaelsanyoto2187@gmail.com>
2025-12-13 01:15:24 +0700

    Update CourseController.php

--- learnify/app/Http/Controllers/CourseController.php
+++ learnify/app/Http/Controllers/CourseController.php
@@ -10,17 +10,65 @@ class CourseController extends Controller
     public function index(Request $request)
     {
         $query = Course::query();
-
         $search = $request->input('search');
 
         if ($search) {
             $query->where('title', 'LIKE', "%{$search}%")
-                 
                   ->orWhere('description', 'LIKE', "%{$search}%");
         }
 
         $courses = $query->get();
 
         return view('coursecatalog', compact('courses', 'search'));
+    }
+
+
+    public function create()
+    {
+        return view('courses.create');
+    }
+
+
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'title'       => 'required',
+            'slug'        => 'required|unique:courses,slug',
+            'price'       => 'required|numeric',
+            'description' => 'required',
+            'image'       => 'nullable',
+        ]);
+
+        Course::create($attributes);
+
+        return redirect('/courses')->with('success', 'Course created successfully!');
+    }
+
+
+    public function edit(Course $course)
+    {
+        return view('courses.edit', compact('course'));
+    }
+
+    public function update(Request $request, Course $course)
+    {
+        $attributes = $request->validate([
+            'title'       => 'required',
+            'slug'        => 'required',
+            'price'       => 'required|numeric',
+            'description' => 'required',
+            'image'       => 'nullable',
+        ]);
+
+        $course->update($attributes);
+
+        return redirect('/courses')->with('success', 'Course updated successfully!');
+    }
+
+    public function destroy(Course $course)
+    {
+        $course->delete();
+
+        return redirect('/courses')->with('success', 'Course deleted successfully!');
     }
 }
