d1390a9e0b959c0a7452d1a0967b68992835b2ab
nicorobinn123 <159020174+nicorobinn123@users.noreply.github.com>
2025-12-14 01:26:16 +0700

    Update MyCourseController

--- learnify/app/Http/Controllers/MyCourseController.php
+++ learnify/app/Http/Controllers/MyCourseController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 use App\Models\Course;
-
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class MyCourseController extends Controller
 {
-     public function MyCourse()
+    public function MyCourse()
     {
-        $courses = Auth::check() ? Auth::user()->courses : collect(); 
+    $courses = Auth::user()?->courses ?? collect();
 
-        return view('MyCourse', compact('courses'));
+    return view('MyCourse', compact('courses'));
     }
+
 }
