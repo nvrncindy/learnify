e1b68251b725523a551538b2c3737f8d331b3268
Vincentius Justin Osvaldy <mrggguyyy@gmail.com>
2025-12-13 14:59:30 +0700

    Add course enrollment check in MaterialsController

--- learnify/app/Http/Controllers/MaterialsController.php
+++ learnify/app/Http/Controllers/MaterialsController.php
@@ -1,12 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MaterialsController extends Controller
 {
      public function Materials(){
+
         return view("Materials");
     }
+
+    public function details (Course $course){
+        $user = Auth::user();
+        
+        if (!$user -> courses()-> where('course_id', $course->id)->exists()){
+            abort(403, 'You are not enrolled in this course');
+        }
+        return view('Materials', compact('course'));
+    }
+
 }
