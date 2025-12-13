00d23e69d837bb4f139ecbab4e8b22029b74f51b
Vincentius Justin Osvaldy <mrggguyyy@gmail.com>
2025-12-13 07:04:43 +0700

    Add courses relationship to User model

--- learnify/app/Models/User.php
+++ learnify/app/Models/User.php
@@ -46,4 +46,12 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+        public function courses()
+    {
+        return $this->belongsToMany(Course::class, 'user_course')->withTimestamps();
+    }
+
+
+}
+
 }
